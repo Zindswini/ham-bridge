7ae5f02894c4ae6c78b2ea95d1ddda5c1806105e
Zindswini <code@zindswini.com>
2026-05-14 18:44:57 -0500

    Use ESP log function, add esp_codec_dev dependency

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -14,8 +14,12 @@
 #include "esp_system.h"
 #include "esp_log.h"
 
+// U8G2 OLED Graphics Library
 #include "u8g2.h"
 #include "u8g2_esp32_hal.h"
+
+// Espressif I2S Codec Library
+#include "esp_codec_dev.h"
 
 // OLED Display and ES8388 Control
 #define PIN_SDA 0
@@ -25,18 +29,20 @@
 #define PIN_DOUT  19
 #define PIN_LRCLK 18
 #define PIN_DIN   21
-#define PIN_SCLK  5
+#define PIN_SCLK  2
 #define PIN_MCLK  3
+
+static const char* TAG = "MAIN";
 
 void app_main(void)
 {
-    printf("Hello world!\n");
+    ESP_LOGI(TAG, "Hello world!");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
     uint32_t flash_size;
     esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+    ESP_LOGI(TAG, "This is %s chip with %d CPU core(s), %s%s%s%s, ",
            CONFIG_IDF_TARGET,
            chip_info.cores,
            (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
@@ -46,18 +52,18 @@ void app_main(void)
 
     unsigned major_rev = chip_info.revision / 100;
     unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    ESP_LOGI(TAG, "silicon revision v%d.%d, ", major_rev, minor_rev);
     if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
+        ESP_LOGI(TAG, "Get flash size failed");
         return;
     }
 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+    ESP_LOGI(TAG, "%" PRIu32 "MB %s flash", flash_size / (uint32_t)(1024 * 1024),
            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+    ESP_LOGI(TAG, "Minimum free heap size: %" PRIu32 " bytes", esp_get_minimum_free_heap_size());
 
-    printf("Creating LCD HAL\n");
+    ESP_LOGI(TAG, "Creating LCD HAL");
     u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
     u8g2_esp32_hal.bus.i2c.sda = PIN_SDA;
     u8g2_esp32_hal.bus.i2c.scl = PIN_SCL;
@@ -67,16 +73,16 @@ void app_main(void)
 
     u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb); // I2C Callback functions for mapping
     
-    printf("Pre-Initialization\n");
+    ESP_LOGI(TAG, "Pre-Initialization");
     u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
     u8g2_InitDisplay(&u8g2);
-    printf("Initialized display\n");
+    ESP_LOGI(TAG, "Initialized display");
     
     u8g2_SetPowerSave(&u8g2, 0);
     
     while(true)
     {
-        printf("Alive!\n");
+        ESP_LOGI(TAG, "Alive!");
         u8g2_ClearBuffer(&u8g2);
 
         u8g2_SetFont(&u8g2, u8g2_font_helvB08_tr);
