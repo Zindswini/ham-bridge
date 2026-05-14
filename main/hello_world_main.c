2b3673f1c762cd14aa1ec08a44f36398f5c5bf22
Zindswini <code@zindswini.com>
2026-05-13 23:41:07 -0500

    Add u8g2 submodules, OLED hello world code

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -13,6 +13,12 @@
 #include "esp_flash.h"
 #include "esp_system.h"
 
+#include "u8g2.h"
+#include "u8g2_esp32_hal.h"
+
+#define PIN_SDA 2
+#define PIN_SCL 3
+
 void app_main(void)
 {
     printf("Hello world!\n");
@@ -42,11 +48,27 @@ void app_main(void)
 
     printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    printf("Creating LCD HAL\n");
+    u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+    u8g2_esp32_hal.bus.i2c.sda = PIN_SDA;
+    u8g2_esp32_hal.bus.i2c.scl = PIN_SCL;
+    u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+    u8g2_t u8g2; // Structure for holding display state
+
+    u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb); // I2C Callback functions for mapping
+    
+    printf("Pre-Initialization\n");
+    u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
+    u8g2_InitDisplay(&u8g2);
+    printf("Initialized display\n");
+    
+    u8g2_SetPowerSave(&u8g2, 0);
+    u8g2_ClearBuffer(&u8g2);
+
+    u8g2_SetFont(&u8g2, u8g2_font_helvB08_tr);
+    u8g2_DrawButtonUTF8(&u8g2, 64, 50, U8G2_BTN_SHADOW1|U8G2_BTN_HCENTER|U8G2_BTN_BW2, 56, 2, 2, "Testing 12 :3");
+
+    u8g2_SendBuffer(&u8g2);   
+    vTaskDelete(NULL);
 }
