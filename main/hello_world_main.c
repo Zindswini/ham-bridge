fe44af5dd53973429f75163865e73697d46482bb
Zindswini <code@zindswini.com>
2026-05-14 23:21:19 -0500

    WIP Reworking u8g2-hal-idf for esp-idf v6.0

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -1,9 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: CC0-1.0
- */
-
 #include <stdio.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
@@ -64,23 +58,8 @@ void app_main(void)
 
     ESP_LOGI(TAG, "Minimum free heap size: %" PRIu32 " bytes", esp_get_minimum_free_heap_size());
 
-    ESP_LOGI(TAG, "Configuring U8G2 HAL Object");
-    u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
-    u8g2_esp32_hal.bus.i2c.sda = PIN_SDA;
-    u8g2_esp32_hal.bus.i2c.scl = PIN_SCL;
-    u8g2_esp32_hal_init(u8g2_esp32_hal);
-
-    u8g2_t u8g2; // Structure for holding display state
-
-    u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb); // I2C Callback functions for mapping
-    
-    ESP_LOGI(TAG, "Preparing to Initialize U8G2");
-    u8x8_SetI2CAddress(&u8g2.u8x8, 0x78);
-    u8g2_InitDisplay(&u8g2);
-    u8g2_SetPowerSave(&u8g2, 0);
-    ESP_LOGI(TAG, "Initialized U8G2 and Display");
-
-    // Init codec i2s peripheral
+    // Init I2C Master Bus
+    ESP_LOGI(TAG, "Initializing I2C Bus");
     i2c_master_bus_handle_t i2c_bus_handle = NULL;
     i2c_master_bus_config_t i2c_mst_cfg = {
         .i2c_port = I2C_NUM_0,
@@ -91,6 +70,26 @@ void app_main(void)
         .flags.enable_internal_pullup = true
     };
     ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_cfg, &i2c_bus_handle));
+    ESP_LOGI(TAG, "Initialized I2C Bus");
+
+    ESP_LOGI(TAG, "Configuring U8G2 HAL Object");
+    u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
+    u8g2_esp32_hal.bus.i2c.i2c_bus_handle = i2c_bus_handle;
+    u8g2_esp32_hal.bus.i2c.scl = PIN_SCL;
+    u8g2_esp32_hal.bus.i2c.sda = PIN_SDA;
+    u8g2_esp32_hal_init(u8g2_esp32_hal);
+
+    u8g2_t u8g2; // Structure for holding display state
+
+    u8g2_Setup_ssd1306_i2c_128x64_noname_f(&u8g2, U8G2_R0, u8g2_esp32_i2c_byte_cb, u8g2_esp32_gpio_and_delay_cb); // I2C Callback functions for mapping
+    
+    ESP_LOGI(TAG, "Preparing to Initialize U8G2");
+    u8x8_SetI2CAddress(&u8g2.u8x8, (0x78<<1));
+    u8g2_InitDisplay(&u8g2);
+    u8g2_SetPowerSave(&u8g2, 0);
+    ESP_LOGI(TAG, "Initialized U8G2 and Display");
+
+    
 
     // Create I2S control interface with I2C bus handle
 
