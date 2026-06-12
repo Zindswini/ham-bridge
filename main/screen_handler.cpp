7b6c8c310561fa3090d5613f6ca34ec1391e62c8
Zindswini <code@zindswini.com>
2026-06-12 12:04:07 -0500

    Port screen_handler to cpp, adjust build flags

--- main/screen_handler.cpp
+++ main/screen_handler.cpp
@@ -8,32 +8,64 @@
 #include "u8g2.h"
 #include "u8g2_esp32_hal.h"
 #include "u8x8.h"
+#include <array>
+#include <format>
+#include <memory>
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <string>
+#include <sys/types.h>
 
 static const char *TAG = "SCREEN_HANDLER";
+
+enum menu_item_type : uint8_t {
+  MENU_ITEM_TYPE_NUMERICAL = 0,
+  MENU_ITEM_TYPE_TOGGLE = 1,
+};
+
+struct menuListObject {
+  char const *text;
+  menu_item_type item_type;
+  bool interactable;
+  bool highlighted;
+  void *value;
+};
+
+struct iconListObject {
+  uint8_t *icon;
+  uint32_t size;
+};
+
+struct screenInformation {
+  uint8_t time_hours = 0;
+  uint8_t time_minutes = 0;
+  uint8_t time_seconds = 0;
+
+  std::array<std::shared_ptr<menuListObject>, 10> menuList;
+  std::array<std::shared_ptr<iconListObject>, 4> iconList;
+};
 
 u8g2_t u8g2;
 static struct screenInformation displayState;
 
-void drawScreen() {
+extern "C" void drawScreen() {
   u8g2_ClearBuffer(&u8g2);
   u8g2_SetFont(&u8g2, u8g2_font_helvB08_tr);
 
   // Draw Icons
   int i = 0;
-  while (displayState.iconList[i] != nullptr) {
+  while (displayState.iconList.at(i) != nullptr) {
     // draw icon
   }
 
   // Draw Time
-  char timeString[12];
-  snprintf(timeString, sizeof(timeString), "%02u:%02u:%02u",
-           displayState.time_hours, displayState.time_minutes,
-           displayState.time_seconds);
-  u8g2_DrawStr(&u8g2, (u8g2.width - u8g2_GetStrWidth(&u8g2, timeString)), 8,
-               timeString);
+  std::string timeString =
+      std::format("%02u:%02u:%02u", displayState.time_hours,
+                  displayState.time_minutes, displayState.time_seconds);
+  u8g2_DrawStr(&u8g2,
+               (u8g2.width - u8g2_GetStrWidth(&u8g2, timeString.c_str())), 8,
+               timeString.c_str());
 
   // Draw Horizontal Line
   u8g2_DrawHLine(&u8g2, 0, 10, u8g2.width);
@@ -41,8 +73,9 @@ void drawScreen() {
   // Draw menu items
   i = 0;
 
-  while (displayState.menuList[i] != nullptr) {
-    struct menuListObject *currentListObject = displayState.menuList[i];
+  while (displayState.menuList.at(i) != nullptr) {
+    struct menuListObject *currentListObject =
+        displayState.menuList.at(i).get();
     // Draw box around ACTIVE menu item
     if (currentListObject->highlighted) {
       u8g2_DrawFrame(
@@ -53,20 +86,20 @@ void drawScreen() {
     u8g2_DrawStr(&u8g2, MENU_PADDING,
                  MENU_TOP_PADDING + MENU_PADDING + TEXT_HEIGHT +
                      ((MENU_FRAME_HEIGHT + MENU_SPACING) * i),
-                 displayState.menuList[i]->text);
+                 displayState.menuList.at(i)->text);
 
-    switch (displayState.menuList[i]->item_type) {
+    std::string value_string;
+    switch (displayState.menuList.at(i)->item_type) {
     case MENU_ITEM_TYPE_NUMERICAL:
       // Interpret value as uint32 and
       // Draw number as string on right side
-      char value_string[10];
-      snprintf(value_string, 9, "%lu", (uint32_t)currentListObject->value);
-      u8g2_DrawStr(
-          &u8g2,
-          (u8g2.width - u8g2_GetStrWidth(&u8g2, value_string) - MENU_PADDING),
-          MENU_TOP_PADDING + MENU_PADDING + TEXT_HEIGHT +
-              ((MENU_FRAME_HEIGHT + MENU_SPACING) * i),
-          value_string);
+      value_string = std::format("%lu", currentListObject->value);
+      u8g2_DrawStr(&u8g2,
+                   (u8g2.width - u8g2_GetStrWidth(&u8g2, value_string.c_str()) -
+                    MENU_PADDING),
+                   MENU_TOP_PADDING + MENU_PADDING + TEXT_HEIGHT +
+                       ((MENU_FRAME_HEIGHT + MENU_SPACING) * i),
+                   value_string.c_str());
       break;
     case MENU_ITEM_TYPE_TOGGLE:
       // Interpret value as bool and
@@ -83,7 +116,7 @@ void drawScreen() {
   u8g2_SendBuffer(&u8g2);
 }
 
-void initializeU8G2(i2c_master_bus_handle_t *i2c_bus_handle) {
+extern "C" void initializeU8G2(i2c_master_bus_handle_t *i2c_bus_handle) {
   u8g2_esp32_hal_t u8g2_esp32_hal = U8G2_ESP32_HAL_DEFAULT;
   u8g2_esp32_hal.bus.i2c.i2c_bus_handle = *i2c_bus_handle;
   u8g2_esp32_hal_init(u8g2_esp32_hal);
@@ -103,7 +136,8 @@ void initializeU8G2(i2c_master_bus_handle_t *i2c_bus_handle) {
   displayState.time_minutes = 11;
   displayState.time_seconds = 12;
 
-  struct menuListObject *firstOption = malloc(sizeof(struct menuListObject));
+  std::shared_ptr<menuListObject> firstOption =
+      std::make_shared<menuListObject>();
   firstOption->text = "List item 1";
   firstOption->item_type = MENU_ITEM_TYPE_NUMERICAL,
   firstOption->highlighted = false, firstOption->interactable = false,
@@ -113,7 +147,7 @@ void initializeU8G2(i2c_master_bus_handle_t *i2c_bus_handle) {
   displayState.menuList[2] = firstOption;
 }
 
-void screenRefreshTask() {
+extern "C" void screenRefreshTask() {
   while (1) {
     drawScreen();
     vTaskDelay(pdMS_TO_TICKS(100));
--- main/screen_handler.h
+++ main/screen_handler.h
@@ -2,8 +2,6 @@
 #define SCREEN_HANDLER_H
 
 #include "driver/i2c_types.h"
-#include "u8g2.h"
-#include <stdint.h>
 
 #define MENU_TOP_PADDING 12
 #define MENU_SPACING 2
@@ -11,36 +9,16 @@
 #define TEXT_HEIGHT 8
 #define MENU_FRAME_HEIGHT (TEXT_HEIGHT + (2 * MENU_PADDING))
 
-typedef enum {
-  MENU_ITEM_TYPE_NUMERICAL = 0,
-  MENU_ITEM_TYPE_TOGGLE = 1,
-} menu_item_type;
-
-struct menuListObject {
-  char *text;
-  menu_item_type item_type;
-  bool interactable;
-  bool highlighted;
-  void *value;
-};
-
-struct iconListObject {
-  uint8_t *icon;
-  uint32_t size;
-};
-
-struct screenInformation {
-  uint8_t time_hours;
-  uint8_t time_minutes;
-  uint8_t time_seconds;
-
-  struct menuListObject *menuList[10];
-  struct iconListObject *iconList[4];
-};
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 void drawScreen();
 
 void initializeU8G2(i2c_master_bus_handle_t *i2c_bus_handle);
 void screenRefreshTask(void);
+#ifdef __cplusplus
+}
+#endif
 
 #endif // SCREEN_HANDLER_H
