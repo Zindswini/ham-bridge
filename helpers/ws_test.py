01f756756df557e840e229849f91a7bf256a836a
Zindswini <code@zindswini.com>
2026-06-18 18:41:31 -0500

    remove separate wss keep alive file, replace c with c++ memory management in wss, ws_test python script

---
+++ helpers/ws_test.py
@@ -0,0 +1,22 @@
+import pathlib
+import ssl
+
+from websockets.sync.client import connect
+
+bridge_ip = "10.42.0.207"
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+localhost_pem = pathlib.Path(__file__).with_name("ham-bridge.pem")
+ssl_context.load_verify_locations(localhost_pem)
+ssl_context.check_hostname = False;
+
+def hello():
+    with connect(f"wss://10.42.0.207/ws", ssl=ssl_context, ping_timeout=None, close_timeout=None) as websocket:
+        websocket.send("Hello world!")
+        print("Sent Hello World")
+
+        while True:
+            message = websocket.recv()
+            print(f"Received {message}")
+
+hello()
--- main/http_server.cpp
+++ main/http_server.cpp
@@ -1,11 +1,5 @@
 #include "http_server.h"
 
-#include <array>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
 #include <esp_err.h>
 #include <esp_eth_com.h>
 #include <esp_event.h>
@@ -14,7 +8,7 @@
 #include <esp_log.h>
 #include <esp_system.h>
 #include <memory>
-#include <sys/cdefs.h>
+#include <vector>
 
 #include "config.h"
 #include "esp_netif_types.h"
@@ -22,7 +16,6 @@
 #include "freertos/projdefs.h"
 
 #include "key_cert_manager.h"
-#include "wss_keep_alive.h"
 
 // Implementation Reference:
 // https://github.com/espressif/esp-idf/blob/v6.0.1/examples/protocols/https_server/wss_server/main/wss_server_example.c
@@ -36,9 +29,14 @@ static esp_err_t wsHandler(httpd_req_t *req) {
     return ESP_OK;
   }
 
-  httpd_ws_frame_t ws_pkt;
-  uint8_t *buf = nullptr;
-  memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+  httpd_ws_frame_t ws_pkt = {
+      .final = false,
+      .fragmented = false,
+      .type = HTTPD_WS_TYPE_CONTINUE,
+      .payload = nullptr,
+      .len = 0,
+  };
+  std::vector<uint8_t> buf;
 
   // Call receive w/ len 0 to extract just payload size
   esp_err_t ret = httpd_ws_recv_frame(req, &ws_pkt, 0);
@@ -51,37 +49,26 @@ static esp_err_t wsHandler(httpd_req_t *req) {
   ESP_LOGI(tag, "Frame length is %d", ws_pkt.len);
   if (ws_pkt.len != 0) {
     // Allocate memory for the payload
-    buf = static_cast<uint8_t *>(calloc(1, (ws_pkt.len + 1)));
-    if (buf == nullptr) {
-      ESP_LOGE(tag, "Failed to calloc memory for buf with len %d",
-               ws_pkt.len + 1);
-      return ESP_ERR_NO_MEM;
-    }
+    buf.resize(ws_pkt.len);
 
-    ws_pkt.payload = buf;
+    ws_pkt.payload = buf.data();
     // Call receive w/ actual len, writes into allocated memory
     ret = httpd_ws_recv_frame(req, &ws_pkt, ws_pkt.len);
     if (ret != ESP_OK) {
       ESP_LOGE(tag, "httpd_ws_recv_frame failed with %d", ret);
-      free(buf);
       return ret;
     }
   }
 
-  // Now have payload written to ws_pkt
-
-  // Handle PONG message
-  if (ws_pkt.type == HTTPD_WS_TYPE_PONG) {
+  // Now have metadata and payload written to ws_pkt and buf
+  switch (ws_pkt.type) {
+  case HTTPD_WS_TYPE_PONG:
     ESP_LOGD(tag, "Received PONG message");
-    free(buf);
-    return wssKeepAliveClientIsActive(
-        static_cast<wss_keep_alive_t>(httpd_get_global_user_ctx(req->handle)),
-        httpd_req_to_sockfd(req));
-  }
-
-  // If TEXT message, echo it back (for now) TODO:
-  if (ws_pkt.type == HTTPD_WS_TYPE_TEXT || ws_pkt.type == HTTPD_WS_TYPE_PING ||
-      ws_pkt.type == HTTPD_WS_TYPE_CLOSE) {
+    // TODO(Zindswini): Actually mark connection as healthy
+    break;
+  case HTTPD_WS_TYPE_TEXT:
+  case HTTPD_WS_TYPE_PING:
+  case HTTPD_WS_TYPE_CLOSE:
     if (ws_pkt.type == HTTPD_WS_TYPE_TEXT) {
       ESP_LOGI(tag, "Received packet with message %s", ws_pkt.payload);
     } else if (ws_pkt.type == HTTPD_WS_TYPE_PING) {
@@ -99,119 +86,83 @@ static esp_err_t wsHandler(httpd_req_t *req) {
     ESP_LOGI(tag, "ws_handler: httpd_handle_t=%p, sockfd=%d, client_info=%d",
              req->handle, httpd_req_to_sockfd(req),
              httpd_ws_get_fd_info(req->handle, httpd_req_to_sockfd(req)));
-    free(buf);
     return ret;
+
+    break;
+  case HTTPD_WS_TYPE_CONTINUE:
+  case HTTPD_WS_TYPE_BINARY:
+    ESP_LOGI(tag, "HTTP Payload type has no implementation!");
+    break;
   }
 
-  free(buf);
   return ESP_OK;
-}
-
-esp_err_t wssOpenFd(httpd_handle_t httpd_handle, int sockfd) {
-  ESP_LOGI(tag, "New client connected %d", sockfd);
-
-  auto *keep_alive_inst =
-      static_cast<wss_keep_alive_t>(httpd_get_global_user_ctx(httpd_handle));
-  return wssKeepAliveAddClient(keep_alive_inst, sockfd);
-}
-
-void wssCloseFd(httpd_handle_t httpd_handle, int sockfd) {
-  ESP_LOGI(tag, "Client disconnected %d", sockfd);
-
-  auto *keep_alive_inst =
-      static_cast<wss_keep_alive_t>(httpd_get_global_user_ctx(httpd_handle));
-  wssKeepAliveRemoveClient(keep_alive_inst, sockfd);
-  fclose((FILE *)sockfd);
 }
 
 static const httpd_uri_t kWsUriConf = {
     .uri = "/ws",
     .method = HTTP_GET,
     .handler = wsHandler,
-    .user_ctx = NULL,
+    .user_ctx = nullptr,
     .is_websocket = true,
-    .handle_ws_control_frames = true,
+    .handle_ws_control_frames = false,
     .supported_subprotocol = nullptr,
 };
 
 static void sendHello(void *arg) {
-  static const char *data = "Hello client!";
-  auto *resp_arg = static_cast<AsyncRespArg *>(arg);
+  std::string data_string = std::string("Hello Client!");
+  std::vector<uint8_t> data_vec(data_string.begin(), data_string.end());
 
-  httpd_handle_t httpd_handle = resp_arg->hd;
-  int file_desc = resp_arg->fd;
-  httpd_ws_frame_t ws_pkt;
-  memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+  // Take ownership of data at arg (cleaned up automatically)
+  auto resp_arg =
+      std::unique_ptr<AsyncRespArg>(static_cast<AsyncRespArg *>(arg));
 
-  ws_pkt.payload = (uint8_t *)data;
-  ws_pkt.len = strlen(data);
-  ws_pkt.type = HTTPD_WS_TYPE_TEXT;
+  httpd_ws_frame_t ws_pkt = {
+      .final = true,
+      .fragmented = false,
+      .type = HTTPD_WS_TYPE_TEXT,
+      .payload = data_vec.data(),
+      .len = data_vec.size(),
+  };
 
-  httpd_ws_send_frame_async(httpd_handle, file_desc, &ws_pkt);
-  free(resp_arg);
+  esp_err_t ret =
+      httpd_ws_send_frame_async(resp_arg->hd, resp_arg->fd, &ws_pkt);
+  if (ret != ESP_OK) {
+    ESP_LOGE(tag, "Failed to send websocket frame! ret = %d", ret);
+  }
 }
 
 static void sendPing(void *arg) {
   auto *resp_arg = static_cast<AsyncRespArg *>(arg);
 
-  httpd_handle_t httpd_handle = resp_arg->hd;
-  int file_desc = resp_arg->fd;
-  httpd_ws_frame_t ws_pkt;
-  memset(&ws_pkt, 0, sizeof(httpd_ws_frame_t));
+  httpd_ws_frame_t ws_pkt = {.final = true,
+                             .fragmented = false,
+                             .type = HTTPD_WS_TYPE_PING,
+                             .payload = nullptr,
+                             .len = 0};
 
-  ws_pkt.payload = nullptr;
-  ws_pkt.len = 0;
-  ws_pkt.type = HTTPD_WS_TYPE_PING;
-
-  httpd_ws_send_frame_async(httpd_handle, file_desc, &ws_pkt);
-  free(resp_arg);
-}
-
-bool clientNotAliveCallback(wss_keep_alive_t keep_alive_handle, int file_desc) {
-  ESP_LOGE(tag, "Client not alive, closing fd %d", file_desc);
-  esp_err_t ret = httpd_sess_trigger_close(
-      wssKeepAliveGetUserCtx(keep_alive_handle), file_desc);
+  esp_err_t ret =
+      httpd_ws_send_frame_async(resp_arg->hd, resp_arg->fd, &ws_pkt);
   if (ret != ESP_OK) {
-    ESP_LOGE(tag, "Failed to close wss fd %d", file_desc);
-    return false;
+    ESP_LOGE(tag, "Failed to send websocket frame! ret = %d", ret);
   }
-  return true;
 }
 
-bool checkClientAliveCallback(wss_keep_alive_t keep_alive_handle,
-                              int file_desc) {
-  ESP_LOGD(tag, "Checking if client (fd=%d) is alive", file_desc);
-
-  auto *resp_arg =
-      static_cast<AsyncRespArg *>(malloc(sizeof(struct AsyncRespArg)));
-  if (resp_arg == nullptr) {
-    ESP_LOGE(tag, "Failed to allocate memory for wss healthcheck on fd %d",
-             file_desc);
-    assert(resp_arg != nullptr);
-  }
-  resp_arg->hd = wssKeepAliveGetUserCtx(keep_alive_handle);
-  resp_arg->fd = file_desc;
-
-  return httpd_queue_work(resp_arg->hd, sendPing, resp_arg) == ESP_OK;
+esp_err_t wssOpenFd(httpd_handle_t hd, int sockfd) {
+  ESP_LOGI(tag, "New httpd connection with sockfd %d", sockfd);
+  return ESP_OK;
 }
 
-static void startWssEchoServer() {
-  // Prepare keep alive engine
-  wss_keep_alive_config_t keep_alive_config = KEEP_ALIVE_CONFIG_DEFAULT();
-  keep_alive_config.max_clients = MAX_HTTPS_CLIENTS;
-  keep_alive_config.client_not_alive_cb = clientNotAliveCallback;
-  keep_alive_config.check_client_alive_cb = checkClientAliveCallback;
-
-  wss_keep_alive_t keep_alive = wssKeepAliveStart(&keep_alive_config);
+static void startWssServer() {
+  // Prepare web socket server
 
   ESP_LOGI(tag, "Starting HTTPS Websocket Server");
-  esp_err_t ret = 0;
+  esp_err_t ret = ESP_OK;
 
   httpd_ssl_config_t conf = HTTPD_SSL_CONFIG_DEFAULT();
   conf.httpd.max_open_sockets = MAX_HTTPS_CLIENTS;
-  conf.httpd.global_user_ctx = keep_alive;
+  conf.httpd.global_user_ctx = nullptr;
   conf.httpd.open_fn = wssOpenFd;
-  conf.httpd.close_fn = wssCloseFd;
+  // conf.httpd.close_fn = wssCloseFd;
 
   // Ask key_cert_manager to attach cert pointers to config
   ESP_ERROR_CHECK(attachKeyCertBundleToConfig(&conf));
@@ -225,21 +176,20 @@ static void startWssEchoServer() {
 
   ESP_LOGI(tag, "Registering URI Handlers");
   httpd_register_uri_handler(server, &kWsUriConf);
-  wssKeepAliveSetUserCtx(keep_alive, server);
 }
 
-static esp_err_t stopWssEchoServer() {
-  wssKeepAliveStop(
-      static_cast<wss_keep_alive_t>(httpd_get_global_user_ctx(server)));
+static esp_err_t stopWssServer() {
+  ESP_LOGI(tag, "Stopping WebSocket Server");
   return httpd_ssl_stop(server);
 }
 
+// Called when wifi/ethernet disconnects
 static void disconnectHandler(void *arg __unused,
                               esp_event_base_t event_base __unused,
                               int32_t event_id __unused,
                               void *event_data __unused) {
   if (server != nullptr) {
-    esp_err_t ret = stopWssEchoServer();
+    esp_err_t ret = stopWssServer();
     if (ret == ESP_OK) {
       server = nullptr;
       ESP_LOGI(tag, "Successfully stopped wss echo server");
@@ -257,21 +207,23 @@ static void connectHandler(void *arg __unused,
                            void *event_data __unused) {
   if (server == nullptr) {
     ESP_LOGI(tag, "Server setup handler called, starting server");
-    startWssEchoServer();
+    startWssServer();
   } else {
     ESP_LOGE(tag, "connectHandler called but server address was not null!");
   }
 }
 
+// Note this pattern for safe ownership transfer into a C function!!!
 static void wssServerSendMessages() {
   bool send_messages = true;
 
   while (send_messages) {
-    vTaskDelay(pdMS_TO_TICKS(1000));
+    vTaskDelay(pdMS_TO_TICKS(5000));
     if (server == nullptr) {
       ESP_LOGD(tag, "SendMessages looping, server ref is nullptr");
       continue; // Server might not be created
     }
+    ESP_LOGI(tag, "Sending messages to all http clients");
 
     size_t clients = MAX_HTTPS_CLIENTS;
     std::array<int, MAX_HTTPS_CLIENTS> client_fds{};
@@ -279,17 +231,24 @@ static void wssServerSendMessages() {
     if (httpd_get_client_list(server, &clients, client_fds.data()) == ESP_OK) {
       for (size_t i = 0; i < clients; i++) {
         int sock = client_fds.at(i);
+
         if (httpd_ws_get_fd_info(server, sock) == HTTPD_WS_CLIENT_WEBSOCKET) {
-          ESP_LOGI(tag, "Active client: fd=%d -> sending async message", sock);
-          auto resp_arg = std::shared_ptr<AsyncRespArg>();
-          assert(resp_arg != nullptr);
+          ESP_LOGI(tag, "Websocket client: fd=%d -> sending async message",
+                   sock);
+
+          auto resp_arg = std::unique_ptr<AsyncRespArg>();
+
           resp_arg->hd = server;
           resp_arg->fd = sock;
-          if (httpd_queue_work(resp_arg->hd, sendHello, resp_arg.get()) !=
-              ESP_OK) {
+
+          esp_err_t ret =
+              httpd_queue_work(resp_arg->hd, sendHello, resp_arg.get());
+
+          if (ret == ESP_OK) {
+            resp_arg.release();
+          } else {
             ESP_LOGE(tag, "httpd_queue_work failed");
             send_messages = false;
-            break;
           }
         }
       }
